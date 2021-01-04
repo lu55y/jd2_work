7b127e61a3c027705b30479102489156870f8c4c
lu55y <mu11er100500@gmail.com>
2021-01-04 21:25:26 +0300

    calss work 04.01.2021

--- tomcat-quickstart/src/main/java/it/academy/servlet/EchoServlet.java
+++ tomcat-quickstart/src/main/java/it/academy/servlet/EchoServlet.java
@@ -1,10 +1,10 @@
 package it.academy.servlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
@@ -12,8 +12,20 @@ import java.util.Date;
 @WebServlet(name = "echoServlet", urlPatterns = "/echo")
 public class EchoServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final PrintWriter writer = resp.getWriter();
-        writer.println("Hello from Echo servlet!"+ new Date());
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            final HttpSession session = req.getSession();
+            if(session.getAttribute("timestamp")==null){
+                session.setAttribute("timestamp",System.currentTimeMillis());
+            }
+            final String id=session.getId();
+            final PrintWriter writer = resp.getWriter();
+            writer.println("Hello from Echo servlet!"+ new Date());
+            writer.println("My session ID= "+ id + "\n"
+                    + session.getAttribute("timestamp"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
